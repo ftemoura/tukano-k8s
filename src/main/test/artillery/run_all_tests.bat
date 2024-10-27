ed01c4b2de794774684b1c4ecc33ec52c3199c81
Guilherme Fernandes <84087774+Gui28F@users.noreply.github.com>
2024-10-27 19:19:05 +0000

    updated script to run all tests
    Co-Authored-By: francisco <113689322+ftemoura@users.noreply.github.com>

--- src/main/test/artillery/run_all_tests.bat
+++ src/main/test/artillery/run_all_tests.bat
@@ -38,6 +38,9 @@ if !errorOccurred! neq 0 exit /b 1
 call :runTest "user POST CSV users again" ./user/post_csv_users.yaml
 if !errorOccurred! neq 0 exit /b 1
 
+call :runTest "follow user" ./shorts/follow_user.yaml
+if !errorOccurred! neq 0 exit /b 1
+
 call :runTest "get followers" ./shorts/get_followers.yaml
 if !errorOccurred! neq 0 exit /b 1
 
@@ -53,7 +56,7 @@ if !errorOccurred! neq 0 exit /b 1
 call :runTest "create short CSV users again" ./shorts/create_short_csv_users.yaml
 if !errorOccurred! neq 0 exit /b 1
 
-call :runTest "delete CSV users by ID" ./user/delete_csv_users_by_id.yaml
+call :runTest "delete CSV users by ID" ./user/delete_csv_users.yaml
 if !errorOccurred! neq 0 exit /b 1
 
 echo All tests completed successfully.
@@ -64,7 +67,7 @@ echo Running %~1 test...
 call artillery run %~2 > %OUTPUT_LOG% 2> %ERROR_LOG%
 type %OUTPUT_LOG%
 if exist %OUTPUT_LOG% (
-    type %OUTPUT_LOG% | findstr /i "not ok" >nul
+    type %OUTPUT_LOG% | findstr /c:"not ok" >nul
     if !errorlevel! equ 0 (
         echo Error found in %~1 test.
         set errorOccurred=1
